ff8f8287132385a2c3433ffe7a22013da7960b04
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2022-01-26 22:38:41 +0530

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,22 @@
+// Get HTML head element
+var head = document.getElementsByTagName('HEAD')[0];
+
+// Create new link Element
+var link = document.createElement('link');
+
+// set the attributes for link element
+link.rel = 'stylesheet';
+
+link.type = 'text/css';
+
+link.href = 'typewriter.css';
+
+// Append link element to HTML head
+head.appendChild(link);
+
+function typewriter(text) {
+const typeWriter = document.getElementById('typewriter-text');
+
+typeWriter.innerHTML = text;
+typeWriter.style.setProperty('--characters', text.length);
+}
