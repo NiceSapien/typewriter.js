dfe6594cead43fa0a9351108c5bd94208955d846
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2022-01-27 10:26:06 +0530

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -8,8 +8,8 @@ var link = document.createElement('link');
 link.rel = 'stylesheet';
 
 link.type = 'text/css';
-console.log
-link.href = 'https://nicesapien.github.io/typewriter.js/typewriter.css';
+
+link.href = 'https://cdn.jsdelivr.net/gh/NiceSapien/typewriter.js@e7e27456d4bf70b25197ab7e5f39df84f2e1fe1d/typewriter.css';
 
 // Append link element to HTML head
 head.appendChild(link);
