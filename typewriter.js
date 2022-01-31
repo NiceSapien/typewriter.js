645ce5484aa190c710e7c56e1eb801c6941bf96a
NiceSapien <71333407+NiceSapien@users.noreply.github.com>
2022-01-31 09:40:06 +0000

    just a update for easier understanding of code

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,7 @@
 // Get HTML head element
 var head = document.getElementsByTagName('HEAD')[0];
 
-// Create new link Element
+// Create new link Element to insert typewriter CSS file.
 var link = document.createElement('link');
 
 // set the attributes for link element
@@ -9,18 +9,23 @@ link.rel = 'stylesheet';
 
 link.type = 'text/css';
 
-link.href = 'https://cdn.jsdelivr.net/npm/@nicesapien0/typewriter.js/typewriter.css';
+link.href = 'https://cdn.jsdelivr.net/npm/@nicesapien0/typewriter.js/typewriter.js';
 
 // Append link element to HTML head
 head.appendChild(link);
-
-var typewriterConfig = ["Your Text", true];
+// default typewriter configuration
+var typewriterConfig = new Array("Your Text", true);
+// main div
 const tYpewriter = document.getElementsByClassName('typewriter-effect');
+// text div
+const typeWriter = document.getElementById('typewriter-text');
+// execute typewriter
 function typewriter() {
-  const typeWriter = document.getElementById('typewriter-text');
-  if (typewriterConfig[1] == true) {
-    tYpewriter[tYpewriter.length - 1].style.setProperty('justify-content', 'center');
-  }
-  typeWriter.innerHTML = typewriterConfig[0];
-  typeWriter.style.setProperty('--characters', typewriterConfig[0].length);
+	// display text in center if specified
+	if (typewriterConfig[1] == true) {
+		tYpewriter[tYpewriter.length - 1].style.setProperty('justify-content', 'center');
+	}
+	// set the text
+	typeWriter.innerHTML = typewriterConfig[0];
+	typeWriter.style.setProperty('--characters', typewriterConfig[0].length);
 }
